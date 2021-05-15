aa740bfe3182edd3c34e212f2488aa63a1401dfc
jariza28 <jefferariza@outlook.com>
2021-05-15 16:10:44 -0500

    first commit

---
+++ Modelos/Productos.cs
@@ -0,0 +1,9 @@
+﻿namespace webapi_csharp.Modelos
+{
+    public class Productos
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Precio { get; set; }
+    }
+}
